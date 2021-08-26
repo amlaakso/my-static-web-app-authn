dcecbc8d82409225994390d50c40ac108845993f
Antti Laaksonen <antti.laaksonen@wibax.com>
2021-08-26 12:58:29 +0300

    add auth

--- svelte-app/src/components/NavBar.svelte
+++ svelte-app/src/components/NavBar.svelte
@@ -1,5 +1,24 @@
 <script>
   import { Link } from 'svelte-routing';
+  import { onMount } from 'svelte';
+
+  let userInfo = undefined;
+  const provider = 'aad';
+  const redirect = window.location.pathname;
+
+  onMount(async () => (userInfo = await getUserInfo()));
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile!');
+      return undefined;
+    }
+  }
 
   function getProps({ href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent;
@@ -20,4 +39,32 @@
       <Link to="/about" {getProps}>About</Link>
     </ul>
   </nav>
+
+  <!-- Login and logout buttons -->
+  <nav class="menu auth">
+    <p class="menu-label">Auth</p>
+    <div class="menu-list auth">
+      {#if !userInfo}
+        <a
+          href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}
+        >
+          {provider}
+        </a>
+      {/if}
+      {#if userInfo}
+        <div class="user">
+          <p>Welcome</p>
+          <p>{userInfo && userInfo.userDetails}</p>
+          {#each userInfo.userRoles as role}
+            <p>Role: {role}</p>
+          {/each}
+          <p>{userInfo && userInfo.identityProvider}</p>
+        </div>
+        <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>
+          Logout
+        </a>
+      {/if}
+    </div>
+  </nav>
+  <!-- end of login and logout buttons -->
 </div>
